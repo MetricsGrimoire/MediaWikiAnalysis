455d9f9bd0385169ce99f898043f10a0c9a2f626
Alvaro del Castillo <acs@bitergia.com>
2013-11-08 03:59:48 +0100

    All is working now. Just need to clean some debug code, add README.md and we can merge into master.

--- mediawiki_analysis.py
+++ mediawiki_analysis.py
@@ -25,40 +25,18 @@
 #
 
 from optparse import OptionParser
-import os, sys
+import sys
 import MySQLdb
-from datetime import datetime
 import urllib2, urllib
 from xml.dom.minidom import parseString
-
-def read_file(filename):
-    fd = open(filename, "r")
-    lines = fd.readlines()
-    fd.close()
-    return lines
-
-
-def parse_file(filename):
-    date_nick_message = []
-    lines = read_file(filename)
-    for l in lines:
-        # [12:39:15] <wm-bot>  Infobot disabled
-        aux = l.split(" ")
-        time = aux[0]
-        time = time[1:len(time) - 1]
-        nick = (aux[1].split("\t"))[0]
-        nick = nick[1:len(nick) - 1]
-        msg = ' '.join(aux[2:len(aux)])
-        date_nick_message.append([time, nick, msg])
-    return date_nick_message
-
 
 def open_database(myuser, mypassword, mydb):
     con = MySQLdb.Connect(host="127.0.0.1",
                           port=3306,
                           user=myuser,
                           passwd=mypassword,
-                          db=mydb)
+                          db=mydb,
+                          charset='utf8')
     # cursor = con.cursor()
     # return cursor
     return con
@@ -110,82 +88,70 @@ def escape_string (message):
         message = message.replace("'", "\\'")
     return message
  
-def get_last_date(channel, cursor):
-    query =  "SELECT MAX(date) FROM irclog, channels "
-    query += "WHERE irclog.channel_id=channels.id "
-    query += " AND channels.name='"+channel+"'"
+def get_last_date(cursor):
+    query =  "SELECT MAX(date) FROM wiki_pages_revs"
     cursor.execute(query)
     return cursor.fetchone()[0]
 
-def insert_message(cursor, date, nick, message, channel_id):
-    message = escape_string (message)
-    nick = escape_string (nick)
-    q = "insert into irclog (date,nick,message,channel_id) values (";
-    q += "'" + date + "','" + nick + "','" + message + "','"+channel_id+"')"
+def insert_page(cursor, pageid, title):
+    title = escape_string (title)
+    q = "INSERT INTO wiki_pages (page_id,title) ";
+    q += "VALUES ('" + pageid + "','" + title + "')"
     cursor.execute(q)
-    
-def create_tables(cursor, con):
-#    query = "DROP TABLE IF EXISTS irclog"
-#    cursor.execute(query)
-#    query = "DROP TABLE IF EXISTS channels"
-#    cursor.execute(query)
 
-    query = "CREATE TABLE IF NOT EXISTS irclog (" + \
+def insert_revision(cursor, pageid, revid, user, date, comment):
+    comment = escape_string (comment)
+    user = escape_string (user)
+    q = "INSERT INTO wiki_pages_revs (page_id,rev_id,date,user,comment) ";
+    q += "VALUES (";
+    q += "'" + pageid + "','" + revid + "','" + date + "','";
+    q +=  user + "','"+ comment +"')"
+    if (opts.debug): print(q)
+    cursor.execute(q)
+
+def create_tables(cursor, con):
+    query = "CREATE TABLE IF NOT EXISTS wiki_pages_revs (" + \
            "id int(11) NOT NULL AUTO_INCREMENT," + \
-           "nick VARCHAR(255) NOT NULL," + \
+           "rev_id int," + \
+           "page_id int," + \
+           "user VARCHAR(255) NOT NULL," + \
            "date DATETIME NOT NULL," + \
-           "message TEXT," + \
-           "channel_id int," + \
+           "comment TEXT," + \
            "PRIMARY KEY (id)" + \
            ") ENGINE=MyISAM DEFAULT CHARSET=utf8"
     cursor.execute(query)
-    
-    query = "CREATE TABLE IF NOT EXISTS channels (" + \
+
+    query = "CREATE TABLE IF NOT EXISTS wiki_pages (" + \
+           "page_id int(11)," + \
+           "title VARCHAR(255) NOT NULL," + \
+           "PRIMARY KEY (page_id)" + \
+           ") ENGINE=MyISAM DEFAULT CHARSET=utf8"
+    cursor.execute(query)
+
+    query = "CREATE TABLE IF NOT EXISTS people (" + \
            "id int(11) NOT NULL AUTO_INCREMENT," + \
            "name VARCHAR(255) NOT NULL," + \
            "PRIMARY KEY (id)" + \
            ") ENGINE=MyISAM DEFAULT CHARSET=utf8"
     cursor.execute(query)
-    
-    try:
-        query = "DROP INDEX ircnick ON irclog;"
-        cursor.execute(query)
-        query = "CREATE INDEX ircnick ON irclog (nick);"
-        cursor.execute(query)
-        con.commit()
-    except MySQLdb.Error:
-        print "Problems creating nick index"
-
     return
-
-def get_url_id(name, cursor):
-    cursor.execute("SELECT * from channels where name='"+name+"'")
-    results =  cursor.fetchall()
-    if len(results) == 0:
-        query = "INSERT INTO channels (name) VALUES ('"+name+"')"
-        cursor.execute(query)
-        cursor.execute("SELECT * from channels where name='"+name+"'")
-        results =  cursor.fetchall()
-    url_id = str(results[0][0])
-    return url_id
 
 if __name__ == '__main__':
     opts = None
     opts = read_options()
 
     # Persistance Layer
-    if False:
-        con = open_database(opts.dbuser, opts.dbpassword, opts.dbname)
-        cursor = con.cursor()
-        create_tables(cursor, con)
-        url_id = get_url_id(opts.url, cursor)
-        last_date = get_last_date(opts.channel, cursor)
+    con = open_database(opts.dbuser, opts.dbpassword, opts.dbname)
+    cursor = con.cursor()
+    create_tables(cursor, con)
+    # Incremental support
+    # last_date = get_last_date(opts.channel, cursor)
 
-    count_msg = count_msg_new = count_msg_drop = count_files_drop = 0
+    count_revs = count_pages = 0
 
     # http://openstack.redhat.com/api.php?action=query&list=allpages&aplimit=500
     api_url = opts.url + "/" + "api.php"
-    print("API URL: " + api_url)
+    if (opts.debug): print("API URL: " + api_url)
 
     # Read all WikiMedia pages and the analyze all revisions
     allpages_query = "action=query&list=allpages&aplimit=500&format=xml"
@@ -197,55 +163,28 @@ if __name__ == '__main__':
         pages_list = pages.read().strip('\n')
     xmlpages = parseString(pages_list)
     for page in xmlpages.getElementsByTagName('p'):
+        count_pages += 1
         # TODO: we can join several title pages here
-        urltitle = urllib.urlencode({'titles':page.attributes['title'].value})
+        title = page.attributes['title'].value
+        urltitle = urllib.urlencode({'titles':title})
         page_allrevs_query = "action=query&prop=revisions&"+urltitle+"&rvlimit=500&format=xml"
         pageid = page.attributes['pageid'].value
-        print(api_url+"?"+page_allrevs_query)
+        insert_page (cursor, pageid, title)
+        if (opts.debug): print(api_url+"?"+page_allrevs_query)
         page_revs = urllib2.urlopen(api_url+"?"+page_allrevs_query)
         page_revs_list = page_revs.read().strip('\n')
         xmlrevs = parseString(page_revs_list)
         for rev in xmlrevs.getElementsByTagName('rev'):
+            count_revs += 1
             revid = rev.attributes['revid'].value
             user = rev.attributes['user'].value
             timestamp = rev.attributes['timestamp'].value
             comment = rev.attributes['comment'].value
-            print (revid+" "+user+" "+timestamp+" "+comment)
-
-    files = os.listdir(opts.data_dir)
-    for logfile in files:
-        year = logfile[0:4]
-        month = logfile[4:6]
-        day = logfile[6:8]
-        date = year + "-" + month + "-" + day
-        try:
-            date_test = datetime.strptime(date, '%Y-%m-%d')
-        except ValueError:
-            count_files_drop += 1
-            print "Bad filename format in  " + logfile
-            continue
-
-        date_nick_msg = parse_file(opts.data_dir + "/" + logfile)
-
-        for i in date_nick_msg:
-            count_msg += 1
-            # date: 2013-07-11 15:39:16
-            date_time = date + " " + i[0]
-            try:
-                msg_date = datetime.strptime(date_time, '%Y-%m-%d %H:%M:%S')
-            except ValueError:
-                count_msg_drop += 1
-                print "Bad format in " + date_time + " (" + logfile + ")"
-                continue
-            if (last_date and msg_date <= last_date): continue
-            insert_message (cursor, date_time, i[1], i[2], url_id)
-            count_msg_new += 1
-            if (count_msg % 1000 == 0): print (count_msg)
+            if (opts.debug): print (pageid +" " + revid+" "+user+" "+timestamp+" "+comment)
+            insert_revision (cursor, pageid, revid, user, timestamp, comment)
+            if (count_revs % 1000 == 0): print (count_revs)
         con.commit()
-
     close_database(con)
-    print("Total messages: %s" % (count_msg))
-    print("Total new messages: %s" % (count_msg_new))
-    print("Total drop messages: %s" % (count_msg_drop))
-    print("Total log files drop: %s" % (count_files_drop))
+    print("Total revisions: %s" % (count_revs))
+    print("Total pagess: %s" % (count_pages))
     sys.exit(0)
